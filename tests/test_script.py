3fd7e52b2e8db51aca3f465801ff4cc5048e536b
Julia Paraizo <juliaparaizo@Julias-MacBook-Air.local>
2024-02-07 21:05:41 -0700

    commited some changes

---
+++ tests/test_script.py
@@ -0,0 +1,13 @@
+import subprocess
+import sys
+
+def test_script_output(capfd):
+    # Run the script file under a new process using the same Python interpreter
+    subprocess.run([sys.executable, './script.py'])
+
+    # Capture process output
+    captured = capfd.readouterr()
+
+    # Assert the output is correct
+    assert captured.out == "Hello World!\n"
+
